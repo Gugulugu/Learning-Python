9c615858788cd6fb6540f93c16e9d0b05965c31b
Gugulugu <denizbickici@mailbox.org>
2021-02-16 21:09:39 +0100

    test

--- Exercise1.py
+++ Exercise1.py
@@ -1,2 +1,14 @@
+from typing import List
+import self as self
 
-#test test test
+#Running sum of an array
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        value = 0
+        array = []
+        for x in nums:
+            value += x
+            array.append(value)
+        return array
+
+print(Solution.runningSum(self,[1, 2, 3, 4])
