25e013cc3b5409ae774f040d1a1ed58c16a002e4
Gugulugu <79146263+Gugulugu@users.noreply.github.com>
2021-03-11 16:33:16 +0100

    update

--- DataScienceTool/Gui.py
+++ DataScienceTool/Gui.py
@@ -1,9 +1,22 @@
 from tkinter import *
+from tkinter import messagebox
+import numpy as np
+import pandas as pd
+
+
+# read in csv file
+def load_file():
+    try:
+        dataset = pd.read_csv(textfield.get())
+        messagebox.showinfo("Message", "File has been loaded successfully in")
+
+    except:
+        messagebox.showinfo("Message", "please enter a valid path")
 
 
 # execute when button pressed
 def button_action():
-    print("test")
+    messagebox.showinfo("Message", "value: " + textfield.get())
 
 
 # create window
@@ -12,10 +25,6 @@ window = Tk()
 window.title("Data Science Tool")
 # window size
 window.geometry("500x500")
-
-# create Buttons
-ReplaceNA_button = Button(window, text="Replace", command=button_action())
-exit_button = Button(window, text="Exit", command=window.quit)
 
 # create Textfield
 textfield = Entry(window, bd=5, width=40)
@@ -28,6 +37,12 @@ textfield_label = Label(window, text="Enter Path of File")
 
 # create Textfield
 textfield = Entry(window, bd=5, width=40)
+
+# create Buttons
+ReplaceNA_button = Button(window, text="Replace", command=button_action)
+exit_button = Button(window, text="Exit", command=window.quit)
+Load_in_file = Button(window, text="Load File", command=load_file)
+
 # positioning of the items
 ReplaceNa_label.grid(row=1, column=1, columnspan=4)
 ReplaceNA_button.grid(row=1, column=10)
@@ -35,6 +50,10 @@ exit_label.grid(row=5, column=1)
 exit_button.grid(row=5, column=10)
 textfield_label.grid(row=9, column=1)
 textfield.grid(row=9, column=10)
+Load_in_file.grid(row=9, column=15)
+
 
 # call window and wait for user to enter
 window.mainloop()
+
+
